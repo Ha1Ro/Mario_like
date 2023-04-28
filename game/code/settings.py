379c11a8a25a9799e4a1601e2f59ea9e7aa70c30
Ha1Ro <hleb.2004@ukr.net>
2023-04-28 21:27:37 +0300

    first commit

---
+++ game/code/settings.py
@@ -0,0 +1,8 @@
+vertical_tile_number = 11
+tile_size = 64 
+
+screen_height = vertical_tile_number * tile_size
+screen_width = 1200
+
+#Path of your computer
+path = '/Users/PC/OneDrive/Рабочий стол/'
